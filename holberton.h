29685451a45f7cd70387cc410cdcfcff2f479a5e
Rafael <r-enc@hotmail.com>
2021-07-23 20:03:24 +0000

    added Header File w/guards

---
+++ holberton.h
@@ -0,0 +1,11 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+#include <stdarg.h>
+#include <limits.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+
+#endif /* _HOLBERTON_H_ */
