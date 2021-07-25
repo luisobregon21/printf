9eb7202ec82a3ab57441c59874a1101cba6a13cc
Rafael <r-enc@hotmail.com>
2021-07-25 16:39:21 +0000

    added printf function

---
+++ test_files/0-printf.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ * _printf - function prints what its passed to it.
+ * currently working for strings and characters.
+ * have not checked edge cases.
+ */
+int _printf(const char *format, ...)
+{
+	/* track variable will keep track of the characters used */
+	int idx, track = 0;
+	va_list list;
+
+	va_start(list, format);
+
+	for (idx = 0; format[idx] != '\0'; idx++)
+	{
+		/* checks for format modifier */
+		if (format[idx] == '%')
+		{
+			/* *
+			 * this code calls the function mod_check
+			 * that checks for the letter that follows
+			 * the modifier (%)
+			 * */
+			track += mod_check(format[idx +1], list);
+			idx++;
+		}
+		else
+		{
+			track += putchar(format[idx]);
+		}
+	}
+	va_end(list);
+	return (track);
+}
+
