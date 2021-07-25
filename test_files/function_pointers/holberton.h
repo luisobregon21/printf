abc89f4975540147c4c06630a074d1536fbfd462
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-07-25 02:09:49 +0000

    added header file with a struct

---
+++ test_files/function_pointers/holberton.h
@@ -0,0 +1,19 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+#include <stdarg.h>
+#include <limits.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int converter(char modifier, va_list list);
+int print_char(va_list list);
+int print_str(va_list list);
+typedef struct converter
+{
+	char mod;
+	/*va_list contains un list de variables*/
+	int (*f)(va_list list);
+} convert;
+#endif /* _HOLBERTON_H_ */
