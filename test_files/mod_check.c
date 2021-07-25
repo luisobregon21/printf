6dec73d81827b985113d992861ee68ce1088bc72
Rafael <r-enc@hotmail.com>
2021-07-25 17:32:21 +0000

    fixed Betty complaints

--- test_files/mod_check.c
+++ test_files/mod_check.c
@@ -12,7 +12,7 @@ int mod_check(char modifier, va_list list)
 	char *str;
 	char letter;
 
-	switch(modifier)
+	switch (modifier)
 	{
 		/* checks for string(s) and character(c) modifiers */
 		case 's':
@@ -21,15 +21,13 @@ int mod_check(char modifier, va_list list)
 		for (index = 0; str[index] != '\0'; index++)
 			putchar(str[index]);
 		return (index);
-		break;
 
 		case 'c':
 			letter = va_arg(list, int);
 			/* prints out the character and returns 1*/
 			putchar(letter);
 			return (1);
-			break;
 	}
 	va_end(list);
-	return(0);
+	return (0);
 }
