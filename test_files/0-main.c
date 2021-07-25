d7abd67988a102efb6ceae4fe4943020cdaaaaf2
Rafael <r-enc@hotmail.com>
2021-07-25 16:11:19 +0000

    created file to check for edge cases

---
+++ test_files/0-main.c
@@ -0,0 +1,36 @@
+#include <limits.h>
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int len;
+	int len2;
+	unsigned int ui;
+	char *p = NULL;
+
+	len = _printf("Let's try to printf a simple sentence.\n");
+	len2 = printf("Let's try to printf a simple sentence.\n");
+	_printf("Character:[%c]\n", 'H');
+	printf("Character:[%c]\n", 'H');
+	_printf("String:[%s]\n", "I am a string !");
+	printf("String:[%s]\n", "I am a string !");
+	//EDGE CASES GOES HERE
+
+	_printf("%%\n");
+	printf("%%\n");
+
+	printf("%s\n", p);
+
+
+
+	//EDGE CASES ENDS
+	
+
+	return (0);
+}
