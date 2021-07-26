0f7d926028028917300d4ab4df3a9b1584b04cef
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-07-26 15:47:46 +0000

    added checks for edge cases and transferred comments to the test file 2-printf.c

--- _printf.c
+++ _printf.c
@@ -1,15 +1,16 @@
 #include "holberton.h"
 /**
  * _printf - function prints what its passed to it.
- * @format: character string that holds '%','c' and 's'.
+ * @format: constant string that is passed to printf.
+ * format is what is inside the "quotes".
+ * it can also hold the especifiers like: '%','c' and 's'.
  * currently working for strings and characters.
  * have not checked edge cases.
  * Return: number of total characters used.
  */
 int _printf(const char *format, ...)
 {
-	/* track variable will keep track of the characters used */
-	int idx, track = 0;
+	int idx, count = 0;
 	va_list list;
 
 	if (format == NULL)
@@ -18,33 +19,32 @@ int _printf(const char *format, ...)
 	if (list == NULL)
 		return (-1);
 
+	/* parsing through the string format to print whats inside of it*/
 	for (idx = 0; format[idx] != '\0'; idx++)
 	{
-		/* checks for format modifier */
-		if (format[idx] == '%')
+	/* checks for format modifier and if there is something after it*/
+		if (format[idx] == '%' && format[idx + 1] != '\0')
 		{
+			/*si lo que sigue es un %, print and sigue contando*/
 			if (format[idx + 1] == '%')
 			{
-				track += _putchar(format[idx]);
+				count += _putchar(format[idx++]);
 			}
 			else
-			/**
-			 * this code calls the function converter
-			 * that checks for the letter that follows
-			 * the modifier (%) and it will use the function
-			 * pointing to that letter.
-			 **/
+		/* calls function converter which calls the respective functiom */
 			{
-				track += converter(format[idx + 1], list);
-				idx++;
+			/* ++idx is equivalent to format[idx + 1] and index++ */
+				count += converter(format[++idx], list);
 			}
 		}
 		else
 		{
-			track += _putchar(format[idx]);
+			/* if the index after the % is nothing, return -1(error) */
+			if (format[idx] == '%' && format[idx + 1] == '\0')
+				return (-1);
+			count += _putchar(format[idx]);
 		}
-
 	}
 	va_end(list);
-	return (track);
+	return (count);
 }
